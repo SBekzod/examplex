24621c86d21ce6bae86fca9c40039de70887ab32
martin <martin@comets.kr>
2024-08-12 22:01:34 +0900

    fix: modify minus process

--- index.js
+++ index.js
@@ -3,5 +3,5 @@ const calt = require("./test/calculator");
 const result1 = calt.add(10, 40);
 console.log("result1:", result1);
 
-const result2 = calt.minus(10, 40);
+const result2 = calt.minus(40, 10);
 console.log("result2:", result2);
