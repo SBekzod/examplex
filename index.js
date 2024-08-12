cf2facf6ac7b72798c07eefe85d654a3ff1c5f54
martin <martin@comets.kr>
2024-08-12 22:00:01 +0900

    feat: develop minus process

--- index.js
+++ index.js
@@ -2,3 +2,6 @@ const calt = require("./test/calculator");
 
 const result1 = calt.add(10, 40);
 console.log("result1:", result1);
+
+const result2 = calt.minus(10, 40);
+console.log("result2:", result2);
