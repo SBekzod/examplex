f4bdbebf95698e0acc0e801e098dc9582c4d537a
martin <martin@comets.kr>
2024-08-12 22:13:24 +0900

    fix: modify multiplication

--- index.js
+++ index.js
@@ -6,5 +6,5 @@ console.log("result1:", result1);
 const result2 = calt.minus(40, 10);
 console.log("result2:", result2);
 
-const result3 = calt.multiply(40, 10);
+const result3 = calt.multiply(400, 100);
 console.log("result3:", result3);
